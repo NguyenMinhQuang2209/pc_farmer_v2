1e0119d2d4665fb505902ddedf4df526f357f45a
Quang Minh <quangnm2209@gmail.com>
2024-03-31 16:03:14 +0700

    fixing player interact

--- Assets/G_Asset/Internal/Scripts/Player/PlayerInteract.cs
+++ Assets/G_Asset/Internal/Scripts/Player/PlayerInteract.cs
@@ -10,6 +10,7 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] private Vector2 offset = new(0.45f, 0.45f);
     private Vector2 interactOffset = Vector2.zero;
 
+    private PlayerToward playerToward = PlayerToward.Bottom;
     private void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
@@ -25,8 +26,17 @@ public class PlayerInteract : MonoBehaviour
     private void Update()
     {
         Vector2 targetPosition = new(transform.position.x + interactOffset.x, transform.position.y + interactOffset.y);
-        targetPosition.x = Mathf.Round(targetPosition.x / 0.16f) * 0.16f;
-        targetPosition.y = Mathf.Round(targetPosition.y / 0.16f) * 0.16f;
+        targetPosition.x = Mathf.Floor(targetPosition.x / 0.16f) * 0.16f;
+        targetPosition.y = Mathf.Floor(targetPosition.y / 0.16f) * 0.16f;
+
+        if (playerToward == PlayerToward.Left)
+        {
+            targetPosition.x += 0.16f;
+        }
+        if (playerToward == PlayerToward.Bottom)
+        {
+            targetPosition.y += 0.16f;
+        }
 
         interactTarget.transform.position = targetPosition;
 
@@ -38,6 +48,7 @@ public class PlayerInteract : MonoBehaviour
     }
     public void ChangePlayerToward(PlayerToward toward)
     {
+        playerToward = toward;
         switch (toward)
         {
             case PlayerToward.Top:
